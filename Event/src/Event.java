9caccfd17932775890d32ec650361b0c1c141d1b
chenzeyu217230118 <161273360+chenzeyu217230118@users.noreply.github.com>
2024-03-12 12:55:21 +0800

    Add files via upload

---
+++ Event/src/Event.java
@@ -0,0 +1,67 @@
+
+import java.util.Date;
+import java.util.ArrayList;
+public class Event {
+   private Date eventID;  
+    private String eventName;  
+    private String eventVenue;  
+    private Date eventDate;  
+    private ArrayList<Attendees> eventAttendees;
+  
+    public  Event() {  
+        eventAttendees = new ArrayList<>();  
+
+    }  
+  
+    public Date getEventID() {  
+        return eventID;  
+    }  
+  
+    public void setEventID(Date string) {  
+        this.eventID = string;  
+    }  
+  
+    public String getEventName() {  
+        return eventName;  
+    }  
+  
+    public void setEventName(String eventName) {  
+        this.eventName = eventName;  
+    }  
+  
+    public String getEventVenue() {  
+        return eventVenue;  
+    }  
+  
+    public void setEventVenue(String eventVenue) {  
+        this.eventVenue = eventVenue;  
+    }  
+  
+    public Date getEventDate() {  
+        return eventDate;  
+    }  
+  
+    public void setEventDate(java.util.Date date) {  
+        this.eventDate = date;  
+    }  
+  
+    public ArrayList<Attendees> getEventAttendees() {
+        return eventAttendees;  
+    }  
+  
+    public void addEventAttendee(Attendees attendee) {
+        eventAttendees.add(attendee);  
+    }  
+  
+    public int getTotalAttendees() {  
+        return eventAttendees.size();  
+    }  
+  
+    public void organizeEvent() {  
+         System.out.println("Total Attendees: " + getTotalAttendees());  
+    }  
+  
+
+}
+ 
+
